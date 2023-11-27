75fdd9bc588162cbc06db1994d6767cf236b9d0c
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2023-11-26 17:28:25 -0800

    added first function to section8

---
+++ R/section8.R
@@ -0,0 +1,22 @@
+
+#' Explode Words into Characters
+#'
+#' This function takes a vector of words and splits each word into its constituent characters.
+#'
+#' @param words A character vector, where each element is a word that you want to split into characters.
+#' @return A list where each element is a character vector corresponding to the characters of a word.
+#' @examples
+#' explode_word(c("tomato"))
+#' explode_word(c("apple", "banana"))
+#' @export
+explode_word <- function(words) {
+  if (!is.character(words)) {
+    stop("The 'words' parameter should be a character vector.")
+  }
+  if (length(words) == 0) {
+    stop("The 'words' parameter should not be empty.")
+  }
+  lapply(words, function(x) strsplit(x, "")[[1]])
+}
+
+
