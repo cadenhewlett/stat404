6fa9ca5d8d6a57d55bfa6bc92d90fa59b4fcbd1e
Caden Hewlett <cadenh139@edu.sd45.bc.ca>
2023-11-26 17:58:59 -0800

    added second function

--- R/section8.R
+++ R/section8.R
@@ -19,4 +19,38 @@ explode_word <- function(words) {
   lapply(words, function(x) strsplit(x, "")[[1]])
 }
 
+#' Multiply Two Words
+#'
+#' This function takes the "product" of two words, combining their letters and dropping duplicates.
+#' It optionally uses the previously defined \code{\link{explode_word}} function to split the words into characters.
+#'
+#' @param word1 The first word (a character string or a vector of characters if `exploded` is TRUE).
+#' @param word2 The second word (a character string or a vector of characters if `exploded` is TRUE).
+#' @param exploded Logical, indicating whether the words are already split into characters.
+#' @return A sorted character vector containing the unique letters from both words, minus duplicates.
+#' @examples
+#' multiply_words(c("A", "B"), c("A", "D"), TRUE) # returns c("B", "D")
+#' multiply_words("Tuhmaytoe", "Tuhmaatoe") # returns c("y")
+#' @seealso \code{\link{explode_word}} for the function used to split words into characters.
+#' @export
+multiply_words <- function(word1, word2, exploded = FALSE) {
+  # Input validation
+  if (!is.character(word1) || !is.character(word2)) {
+    stop("Both 'word1' and 'word2' should be character strings or character vectors.")
+  }
+  if (!is.logical(exploded) || length(exploded) != 1) {
+    stop("'exploded' should be a single logical value.")
+  }
 
+  if (!exploded) {
+    word1 = unlist(explode_word(word1))
+    word2 = unlist(explode_word(word2))
+  }
+
+  word <- unique(c(word1, word2))
+  for (factor in intersect(word1, word2)) {
+    word <- setdiff(word, factor)
+  }
+
+  return(sort(word))
+}
